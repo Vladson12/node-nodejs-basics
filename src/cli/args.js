ba825ddbc337bde32edb5bc2da72e07a89aa0016
Vladislav Magomedov <vladdmagg94@yandex.ru>
2022-12-03 20:56:55 +0300

    Command line interface(src/cli) subtasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  let args = process.argv.slice(2);
+  while (args.length !== 0) {
+    console.log(`${args[0].slice(2)} is ${args[1]}`);
+    args = args.slice(2);
+  }
 };
 
 parseArgs();
