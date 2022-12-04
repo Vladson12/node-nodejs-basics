8d5445ed68e51c318e0d4235c8c675267247983d
Vladislav Magomedov <vladdmagg94@yandex.ru>
2022-12-04 11:03:10 +0300

    Hash (src/hash) subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fsProm from "fs/promises";
+import path from "path";
+import { getFileDir } from "../util/dirAndFileName.js";
+import crypto from "crypto";
+
+const { __dirname } = getFileDir(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+  try {
+    const data = await fsProm.readFile(filePath);
+    const hash = crypto.createHash("sha256").update(data).digest("hex");
+    console.log(hash);
+  } catch (err) {
+    throw err;
+  }
 };
 
 await calculateHash();
