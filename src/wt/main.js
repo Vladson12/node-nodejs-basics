8dc162ec50f019e38ac365d6d18258869f198046
Vladislav Magomedov <vladdmagg94@yandex.ru>
2022-12-05 01:09:22 +0300

    Worker Threads (src/wt) subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,45 @@
+import os from "os";
+import { Worker } from "worker_threads";
+import path from "path";
+import { getFileDir } from "../util/dirAndFileName.js";
+
+const { __dirname } = getFileDir(import.meta.url);
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const cpusNumber = os.cpus().length;
+  const initialFibonacci = 10;
+
+  const workerFile = path.join(__dirname, "worker.js");
+
+  const workerPromises = [];
+  for (let index = 0; index < cpusNumber; index++) {
+    const worker = new Worker(workerFile, {
+      workerData: initialFibonacci + index,
+    });
+
+    workerPromises.push(
+      new Promise((resolve, reject) => {
+        worker.on("message", (msg) => {
+          resolve(msg);
+        });
+        worker.on("error", (err) => reject());
+      })
+    );
+  }
+
+  const workersResult = await Promise.allSettled(workerPromises);
+
+  const result = workersResult.map((res) => {
+    return res.status === "fulfilled"
+      ? {
+          status: "resolved",
+          data: res.value,
+        }
+      : { status: "error", data: null };
+  });
+
+  console.log(result);
 };
 
 await performCalculations();
