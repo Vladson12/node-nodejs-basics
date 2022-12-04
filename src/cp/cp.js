3d0edc396b6941b2db7684f2f863012943c72ee0
Vladislav Magomedov <vladdmagg94@yandex.ru>
2022-12-05 02:44:51 +0300

    Child Processes (src/cp) subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import { fork } from "child_process";
+import path from "path";
+import { getFileDir } from "../util/dirAndFileName.js";
+
+const { __dirname } = getFileDir(import.meta.url);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+  const scriptFile = path.join(__dirname, "files", "script.js");
+
+  const child = fork(scriptFile, args);
 };
 
-spawnChildProcess();
+const args = process.argv.slice(2);
+spawnChildProcess(args);
